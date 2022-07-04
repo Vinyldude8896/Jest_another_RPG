8d0cd121763c152756fa6a25fe0824b7fd2168a9
Kevin Reid <kevinnivek@me.com>
2022-07-04 15:45:24 -0400

    added potion tests and plyer functions

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,4 +1,3 @@
-
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
@@ -65,3 +64,31 @@ test("subtracts from player's health", () => {
 
     expect(player.health).toBe(0);
 });
+
+test("gets player's attach value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
+  });
+
